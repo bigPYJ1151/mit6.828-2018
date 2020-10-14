511b655e0dfbe8d463739f5512b943b23f6bb2e7
bigpyj <bigpyj@foxmail.com>
2020-10-14 17:14:50 +0800

    Finsh HW11

--- homework/hw3/log.c
+++ homework/hw3/log.c
@@ -5,6 +5,8 @@
 #include "sleeplock.h"
 #include "fs.h"
 #include "buf.h"
+#include "mmu.h"
+#include "proc.h"
 
 // Simple logging that allows concurrent FS system calls.
 //
@@ -67,16 +69,20 @@ initlog(int dev)
 
 // Copy committed blocks from log to their home location
 static void
-install_trans(void)
+install_trans(int is_commit)
 {
   int tail;
 
   for (tail = 0; tail < log.lh.n; tail++) {
-    struct buf *lbuf = bread(log.dev, log.start+tail+1); // read log block
     struct buf *dbuf = bread(log.dev, log.lh.block[tail]); // read dst
-    memmove(dbuf->data, lbuf->data, BSIZE);  // copy block to dst
+
+    if (!is_commit){
+      struct buf *lbuf = bread(log.dev, log.start+tail+1); // read log block
+      memmove(dbuf->data, lbuf->data, BSIZE);  // copy block to dst
+      brelse(lbuf);
+    }
+
     bwrite(dbuf);  // write dst to disk
-    brelse(lbuf);
     brelse(dbuf);
   }
 }
@@ -112,11 +118,22 @@ write_head(void)
   brelse(buf);
 }
 
+// bad recover_from_log
+// static void
+// recover_from_log(void)
+// {
+//   read_head();      
+//   cprintf("recovery: n=%d but ignoring\n", log.lh.n);
+//   install_trans();
+//   log.lh.n = 0;
+//   write_head();
+// }
+
 static void
 recover_from_log(void)
 {
   read_head();
-  install_trans(); // if committed, copy from log to disk
+  install_trans(0); // if committed, copy from log to disk
   log.lh.n = 0;
   write_head(); // clear the log
 }
@@ -195,7 +212,7 @@ commit()
   if (log.lh.n > 0) {
     write_log();     // Write modified blocks from cache to log
     write_head();    // Write header to disk -- the real commit
-    install_trans(); // Now install writes to home locations
+    install_trans(1); // Now install writes to home locations
     log.lh.n = 0;
     write_head();    // Erase the transaction from the log
   }
